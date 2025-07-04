06195421b20d35c5257edea28140ae67bfbc2b7b
Alberto Félix Rosas <alberto.felix.js@gmail.com>
2025-07-04 00:13:45 -0700

    Refactor venue management types and services: rename CreateVenueDto to CreateOrUpdateVenueDto, update related functions in venueService, and adjust EditVenue component to handle venue updates correctly.

--- src/services/venueService.ts
+++ src/services/venueService.ts
@@ -1,5 +1,5 @@
 import api from '../api/axios';
-import type { CreateVenueDto, Venue } from '../types/venue';
+import type { CreateOrUpdateVenueDto, Venue } from '../types/venue';
 
 export const getVenues = async (): Promise<Venue[]> => {
   const response = await api.get<Venue[]>("/venues");
@@ -11,7 +11,7 @@ export const getVenueById = async (id: number): Promise<Venue> => {
   return response.data;
 };
 
-export const createVenue = async (data: CreateVenueDto): Promise<Venue> => {
+export const createVenue = async (data: CreateOrUpdateVenueDto): Promise<Venue> => {
   const response = await api.post<Venue>("/venues", data);
   return response.data;
 };
@@ -20,7 +20,6 @@ export const deleteVenue = async (id: number): Promise<void> => {
   await api.delete(`/venues/${id}`);
 };
 
-export const editVenue = async (venue: Venue): Promise<void> => {
-  const { venue_id } = venue;
-  await api.put(`/venues/${venue_id}`, venue);
+export const editVenue = async (id: number, venue: CreateOrUpdateVenueDto): Promise<void> => {
+  await api.put(`/venues/${id}`, venue);
 };
